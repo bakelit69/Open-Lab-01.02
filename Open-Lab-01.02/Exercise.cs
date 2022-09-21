2446093cc35a8f8876955911f137101b0441af62
bakelit69 <113536297+bakelit69@users.noreply.github.com>
2022-09-21 20:41:07 +0200

    Update Exercise.cs

--- Open-Lab-01.02/Exercise.cs
+++ Open-Lab-01.02/Exercise.cs
@@ -1,11 +1,12 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_01._02
 {
     public class Exercise
     {
-        public int Sum(int a, int b)
+        public int Sum(int x, int y)
         {
+            return x + y;
             throw new NotImplementedException();
         }
     }
